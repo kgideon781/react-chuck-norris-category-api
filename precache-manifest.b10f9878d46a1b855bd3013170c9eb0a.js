f536dfece86631db014fb76c0fe236ca69b16043
Giddeon Legacy <kgideon781@gmail.com>
2020-12-16 06:46:46 +0300

    Updates

---
+++ precache-manifest.b10f9878d46a1b855bd3013170c9eb0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6b6fda1439761222e09e261ecb9858",
+    "url": "/react-chuck-norris-category-api/index.html"
+  },
+  {
+    "revision": "7b37822eff0c259f1b4f",
+    "url": "/react-chuck-norris-category-api/static/css/2.df0a272d.chunk.css"
+  },
+  {
+    "revision": "fa89a5efc5885842467a",
+    "url": "/react-chuck-norris-category-api/static/css/main.bd7ac539.chunk.css"
+  },
+  {
+    "revision": "7b37822eff0c259f1b4f",
+    "url": "/react-chuck-norris-category-api/static/js/2.dbc6c55e.chunk.js"
+  },
+  {
+    "revision": "fa89a5efc5885842467a",
+    "url": "/react-chuck-norris-category-api/static/js/main.bc2f7768.chunk.js"
+  },
+  {
+    "revision": "0deea59cff04752a4c3d",
+    "url": "/react-chuck-norris-category-api/static/js/runtime~main.4b5d7d72.js"
+  },
+  {
+    "revision": "956052896ab173087048f34aed49da79",
+    "url": "/react-chuck-norris-category-api/static/media/application.95605289.svg"
+  }
+]);
